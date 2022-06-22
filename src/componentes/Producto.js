4930faf2bd898b7d6fe8362309a860a95149c1c6
AJPiero <ajpiero@users.noreply.github.com>
2022-06-22 18:05:55 -0400

    Primera Entrega

--- src/componentes/Producto.js
+++ src/componentes/Producto.js
@@ -1,23 +1,21 @@
+import { Card, Button } from 'react-bootstrap';
+
 const Producto = () => {
     return (
         <div>   
-<Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
-  <Card.Body>
-    <Card.Title>Card Title</Card.Title>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </Card.Text>
-    <Button variant="primary">Go somewhere</Button>
-  </Card.Body>
-</Card>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Body>
+              <Card.Title>Titulo</Card.Title>
+              <Card.Text>
+                Este es un pequeno texto que sirve como ejemplo para tener contenido en este espacio.
+              </Card.Text>
+              <Button variant="primary">Ir a algun lado</Button>
+            </Card.Body>
+          </Card>
+        </div>       
 
-
-        </div>
-        
-     
-     )
+          )
  }
 
  export default Producto
